f2854cb6fc811591faf9f5a1e7e462855ddb8952
anku-26 <2654anku@gmail.com>
2025-06-08 14:48:38 +0530

    complete

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// @ts-ignore
+import ChatWindow from './components/ChatWindow'
+
+function App() {
+  return (
+    <div className="App">
+      <ChatWindow />
+    </div>
+  );
+}
+
+export default App; 
